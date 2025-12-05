90e9cd35ae9a36b9dff77072022ff03bda3c8082
nikita <nikitaorlov59@gmail.com>
2025-12-05 17:13:39 +0500

    Добавьте файлы проекта.

---
+++ l6/Cat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace l6
+{
+    internal class Cat
+    {
+        private string name;
+        public string Name
+        {
+            get { return $"кот: {name}"; }
+            set
+            {
+                if (value != "" || value != null)
+                {
+                    name = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка имени");
+                    name = "";
+                }
+            }
+        }
+
+        public Cat(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ l6/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace l6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat barsik = new Cat("Барсик");
+            Console.WriteLine(barsik.Name);
+        }
+    }
+}
